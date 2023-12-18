dbc82f70fc700300cf072fe288253bb9f0c109d6
Lucian Miller <lucian.miller185@gmail.com>
2023-12-18 09:33:25 -0800

    Add setup files

---
+++ Palindrome/Models/Palindrome.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+
+namespace Palindrome.Models
+{
+  public class Palindrome
+  {
+    
+  }
+}
---
+++ Palindrome/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Palindrome.Models;
+
+namespace Palindrome
+{
+  class Program
+  {
+    static void Main()
+    {
+      
+    }
+  }
+}
